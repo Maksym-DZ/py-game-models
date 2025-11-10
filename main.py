fd911ea2a9dacca302debfb7d556891261f3f188
Maksym Dazidov <maksymdazidov@gmail.com>
2025-11-10 17:13:31 +0100

    hard code 3

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def main() -> None:
     for name, player_data in players.items():
         guild_data = player_data.get("guild", {})
         if guild_data:
-            guild = Guild.objects.get_or_create(name=guild_data["name"], description=guild_data["description"])[0]
+            guild = Guild.objects.get_or_create(name=guild_data["name"], defaults={"description": guild_data["description"]})[0]
         else:
             guild = None
         race_data = player_data["race"]
-        race = Race.objects.get_or_create(name=race_data["name"], description=race_data["description"])[0]
+        race = Race.objects.get_or_create(name=race_data["name"], defaults={"description": race_data["description"]})[0]
         player = Player.objects.create(
             nickname=name,
             email=player_data["email"],
@@ -22,7 +22,7 @@ def main() -> None:
             guild=guild
         )
         for skill_data in race_data["skills"]:
-            skill = Skill.objects.get_or_create(name=skill_data["name"], bonus=skill_data["bonus"], race=race)[0]
+            skill = Skill.objects.get_or_create(name=skill_data["name"], defaults={"bonus": skill_data["bonus"], "race": race})[0]
 
 
 if __name__ == "__main__":
